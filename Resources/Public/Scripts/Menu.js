2bdb9cbf6f49b5197b89c0f76344ddab3a9371fa
Maciej <maciej@myx.systems>
2020-09-26 10:42:50 +1000

    Initial Commit

---
+++ Resources/Public/Scripts/Menu.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("skyLaunchNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "X" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("skyLaunchNav").style.width = "0";
+}
